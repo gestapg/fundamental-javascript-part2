cd032cec95cf5d5912f5f4c2881aee72cbcbc3d6
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-05-31 22:02:10 +0700

    Activated Strict mode learning

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
+
+// const interface = "audio";
+// const private = 165;
+// const if = 238728
