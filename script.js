11ca155fe2f15876636032d97456ffa5989ea13f
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-01 00:42:06 +0700

    Arrow Function Learning

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 
-*/
+
 
 // declaration function
 function calcAge1(birthYear) {
@@ -51,3 +51,20 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1995);
 
 console.log(age1, age2);
+
+*/
+
+// Arrow Function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1995);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1995, "Gesta"));
+console.log(yearsUntilRetirement(1996, "Sarah"));
