44004d60cfa832a800978dd979c568f22c3a997b
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-05-31 22:26:45 +0700

    Functions Learning

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,28 @@ if (hasDriversLicense) console.log("I can drive!");
 // const interface = "audio";
 // const private = 165;
 // const if = 238728
+
+*/
+
+function logger() {
+  console.log("My name is Gesta");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(4, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
