cea2695ebb5222b3d4a9797e6396464e3029f703
Gesta Putra Gemilang <gestaputrag@gmail.com>
2021-06-02 16:26:53 +0700

    looping assignment

--- assignment.js
+++ assignment.js
@@ -120,4 +120,5 @@ for (let i = 0; i < populations.length; i++) {
 console.log(percentages2);
 
 const listOfNeighbours = [["Canada", "Mexico"], ["Spain"], ["Norway", "Sweden", "Russia"]];
+
 for (let i = 0; i < listOfNeighbours.length; i++) for (let y = 0; y < listOfNeighbours[i].length; y++) console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
